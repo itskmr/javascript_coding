443e482b244333a7a17ad358ee4fc4a3c2212241
Ronit Kumar <roizlive69@gmail.com>
2025-02-11 00:40:59 +0530

    Node basics folder creted and print hello world

---
+++ nodebasics/helloworld.js
@@ -0,0 +1,7 @@
+// basically node use hoti hai to provide run time javascript environment and so that hum javascript ko server 
+// side pe bhi use kar sake
+
+const hello  = "Hello World";
+console.log(hello);
+
+// so is tarah se hum node se javascript code run kar sakte h
